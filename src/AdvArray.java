46d7f3501cd57a0e0d93d1cf5afe3bb64b5ce4f0
ramya276 <32501427+ramya276@users.noreply.github.com>
2017-11-07 15:13:16 -0800

    Adding remaining solns.

--- src/AdvArray.java
+++ src/AdvArray.java
@@ -3,8 +3,16 @@ public class AdvArray {
 public static void main(String[] args) {
 	//maxArray();
 	//minArray();
-	twoDArrayMax();
-	twoDArrayMin();
+	//twoDArrayMax();
+	//twoDArrayMin();
+	//arrayRev();
+	//arraySum();
+	//arrayDiff();
+	//arrayPrintEven();
+	//arrayPrintOdd();
+	//arrayPrintPositive();
+	//arrayPrintNegative();
+	arrayPrintPrime();
 	}
 
 	public static void maxArray()
@@ -72,5 +80,97 @@ public static void main(String[] args) {
 		System.out.println("Final Min Value is "+min);
 				
 	}
+	public static void arrayRev() {
+		int[] arr= {1,2,3,4,5};
+			for(int i=arr.length-1;i>=0;i--) {
+				System.out.println(arr[i]);
+			}
+	}
+	public static void arraySum() {
+		int[] a= {1,2,3,4,5};
+		int[] b= {6,7,8,9,1};
+		int[] sum=new int[5];
+			
+		for(int i=0;i<5;i++) {	
+				sum[i]=a[i]+b[i];
+				System.out.println(sum[i]);
+		}
+	}
 
+	public static void arrayDiff() {
+		int[] a= {1,2,3,4,5};
+		int[] b= {6,7,8,9,1};
+		int[] diff=new int[5];
+			
+		for(int i=0;i<5;i++) {	
+				diff[i]=b[i]-a[i];
+				System.out.println(diff[i]);
+		}
+	}
+	
+	public static void arrayPrintEven() {
+		int[] a= {1,2,3,4,5};
+		
+		for(int i=0;i<5;i++) {
+			if(a[i]%2==0) {
+				System.out.println(a[i]);
+			}
+		}
+	}
+	
+	public static void arrayPrintOdd() {
+		int[] a= {1,2,3,4,5};
+		
+		for(int i=0;i<5;i++) {
+			if(a[i]%2!=0) {
+				System.out.println(a[i]);
+			}
+		}
+	}
+	
+	public static void arrayPrintPositive() {
+		int[] a= {1,-2,3,-4,-5};
+		
+		for(int i=0;i<5;i++) {
+			if(a[i]>0) {
+				System.out.println(a[i]);
+			}
+		}
+	}
+	
+	public static void arrayPrintNegative() {
+		int[] a= {1,-2,3,-4,-5};
+		
+		for(int i=0;i<5;i++) {
+			if(a[i]<0) {
+				System.out.println(a[i]);
+			}
+		}
+	}
+	
+	public static void arrayPrintPrime() {
+		int[] a= new int[25];
+		int i=0;
+		boolean isPrime=true;
+		while(i<=24) {
+				a[i]=i+1;
+				System.out.println(a[i]);
+				i++;
+		}
+			for(i=0;i<=a.length;i++) {
+				for(int j=2;j<=5;j++) {
+					if(a[i]%j!=0) {
+						isPrime=true;
+					}
+					else {
+						isPrime=false;
+					}
+				}
+			}
+				for(i=0;i<a.length;i++) {
+					if(isPrime)
+						System.out.println(a[i]);
+				}
+	}
+	
 }
